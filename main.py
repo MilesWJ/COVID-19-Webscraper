045bc6e2ae6633b6f8160ddab11408370036495b
Miles James <milesw.james@gmail.com>
2021-03-09 09:33:23 -0800

    Update main.py

--- main.py
+++ main.py
@@ -26,27 +26,21 @@ def retrieve_data(data):
 
 
 def decide_data():
-
     while True:
-
         decide = abs(int(input("\n1. Total amount of COVID-19 cases in United States\n2. Total amount of COVID-19 related deaths in the United States\n3. Total amount of administered vaccines in the United States\nEnter the data number you would like to view: ")))
-
         if decide == 1:
             data = retrieve_data(CASES)
             print(f"\nUnited States COVID-19 case count: {data}")
             break
-
         elif decide == 2:
             data = retrieve_data(DEATHS)
             print(f"\nUnited States COVID-19 related death count: {data}")
             break
-
         elif decide == 3:
             data = retrieve_data(VACCINES)
             print(
                 f"\nUnited States COVID-19 vaccine administration count: {data}")
             break
-
         else:
             print("Invalid data selector...")
             continue
@@ -54,7 +48,6 @@ def decide_data():
 
 def display_data():
     print("\nNOTE: All data is from the CDC's website: https://www.cdc.gov/coronavirus/2019-ncov/index.html")
-
     decide_data()
 
 
